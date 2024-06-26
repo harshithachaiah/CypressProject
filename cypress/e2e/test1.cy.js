609506c38ee6245c0d32d51bb60f5aaa8cdeebae
Harshith A G <43842509+harshithachaiah@users.noreply.github.com>
2024-06-26 11:17:32 +0100

    Update test1.cy.js

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -1,8 +1,8 @@
 /// <Reference types ="cypress"/>
 it('Google Search', () => {
     cy.visit('https://google.com')
-    cy.get('#W0wltc > .QS5gu').click()
-    cy.get('#APjFqb').type('Selenium{Enter}')
+    // cy.get('#W0wltc > .QS5gu').click()
+    // cy.get('#APjFqb').type('Selenium{Enter}')
     // cy.get('.aajZCb > .lJ9FBc > center > .gNO89b').click()
 
 })
